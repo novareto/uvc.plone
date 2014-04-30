ec1274b7e380642cde625693c227c3fbea31ef52
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2014-04-30 16:08:45 +0000

    Made the package requirements mandatory

--- uvc/plone/api.py
+++ uvc/plone/api.py
@@ -2,56 +2,42 @@
 # Copyright (c) 2007-2011 NovaReto GmbH
 # cklinger@novareto.de
 
-from uvc.api import log
-from zope import interface
-
-try:
-    import grok
-    GROK = True
-except:
-    GROK = False
-
-log('Running nva.api in GROK Environment --> %s' % GROK)
-
-
-if GROK:
-    from uvcsite import Page
-    from uvcsite import Form
-    import grok
-    from zope.security.interfaces import IPrincipal
-
-    def get_princiapl(context, request):
-        return request.principal
-
-else:
-    from five.grok import View
-    from five import grok
-    from zope.publisher.publish import mapply
-    from zeam.form.plone import Form
-    from zeam.form.base import Fields, action
-    from zope.component import getMultiAdapter
-    from AccessControl.interfaces import IUser as IPrincipal
-    from grokcore.layout import Layout, Page
-    from megrok import pagetemplate as pt
-    from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
-
-    grok.templatedir('templates')
-
-    class Layout(Layout):
-        grok.name('layout')
-        grok.context(interface.Interface)
+from five.grok import View
+from five import grok
+from zope.publisher.publish import mapply
+from zeam.form.plone import Form
+from zeam.form.base import Fields, action
+from zope.component import getMultiAdapter
+from AccessControl.interfaces import IUser as IPrincipal
+from grokcore.layout import Layout as BaseLayout, Page
+from megrok import pagetemplate as pt
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
 
-    def get_principal(context, request):
-        portal_state = getMultiAdapter((context, request), name="plone_portal_state")
-        return portal_state.member().getUser()
+grok.templatedir('templates')
 
-    class Form(Form):
-        """ """
-        grok.baseclass()
 
-    class FormTemplate(pt.PageTemplate):
-        pt.view(Form)
+class Layout(Layout):
+    grok.name('layout')
+    grok.context(interface.Interface)
 
-    class FormMacros(grok.View):
-        grok.context(interface.Interface)
+
+def get_principal(context, request):
+    portal_state = getMultiAdapter(
+        (context, request), name="plone_portal_state")
+    return portal_state.member().getUser()
+
+
+class Form(Form):
+    grok.baseclass()
+
+
+class FormTemplate(pt.PageTemplate):
+    pt.view(Form)
+
+
+class FormMacros(grok.View):
+    grok.context(interface.Interface)
+
+
+__all__ = ['Layout', 'Form', 'Page', 'IPrincipal', 'get_principal']
