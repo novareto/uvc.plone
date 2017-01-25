dd6434310cbd35659adbb894a64e3af5541a3021
Souheil CHELFOUH <trollfot@gmail.com>
2017-01-25 14:38:55 +0100

    Added registry, multiselect, inout widget

---
+++ uvc/plone/widgets/collection.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from five import grok
+from zope.interface import Interface
+from Products.CMFPlone.resources import add_resource_on_request
+from zeam.form.ztk.widgets.choice import ChoiceSchemaField
+from zeam.form.ztk.interfaces import ICollectionSchemaField
+from zeam.form.base.interfaces import IWidget
+from zeam.form.ztk.widgets.collection import (
+    CollectionSchemaField, newCollectionWidgetFactory, MultiChoiceFieldWidget)
+
+
+class Resources(grok.DirectoryResource):
+    grok.path('resources')
+    grok.name('uvc.plone.resources')
+
+
+class PloneFieldWidget(FieldWidget, Acquisition.Explicit):
+    grok.baseclass()
+
+    @property
+    def context(self):
+        # Plone Zope 2 template need a context.
+        return self.form.context
+
+
+class PloneMultiSelectFieldWidget(MultiChoiceFieldWidget, PloneFieldWidget):
+    grok.adapts(CollectionSchemaField, ChoiceSchemaField, Interface, Interface)
+    grok.name('INOUT')
+
+    def __init__(self, field, value_field, form, request):
+        super(PloneMultiSelectFieldWidget, self).__init__(field, form, request)
+        add_resource_on_request(request, 'inout')
+
+
+grok.global_adapter(
+    newCollectionWidgetFactory(mode='INOUT'),
+    adapts=(ICollectionSchemaField, Interface, Interface),
+    provides=IWidget,
+    name='INOUT')
